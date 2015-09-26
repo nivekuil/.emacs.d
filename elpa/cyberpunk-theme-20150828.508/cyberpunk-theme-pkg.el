48eea6e209e429c7dc824654c808b13447f7be0d
Kevin Liu <mail@nivekuil.com>
2015-09-25 19:08:38 -0700

    Update

---
+++ elpa/cyberpunk-theme-20150828.508/cyberpunk-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cyberpunk-theme" "20150828.508" "Cyberpunk Color Theme" 'nil :keywords '("color" "theme" "cyberpunk"))
---
+++ elpa/graphviz-dot-mode-20150525.1000/graphviz-dot-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "graphviz-dot-mode" "20150525.1000" "Mode for the dot-language used by graphviz (att)." 'nil :url "http://ppareit.github.com/graphviz-dot-mode/" :keywords '("mode" "dot" "dot-language" "dotlanguage" "graphviz" "graphs" "att"))
