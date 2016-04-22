7236c8900e39d2fbc314f804965e817e53837226
Kevin Liu <mail@nivekuil.com>
2016-04-21 19:18:00 -0700

    Should finally make opening websites work properly

--- init.el
+++ init.el
@@ -20,13 +20,57 @@
      (output-dvi "xdvi")
      (output-pdf "Okular")
      (output-html "xdg-open"))))
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(inhibit-startup-screen t)
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (git-commit command-log-mode smart-comment swiper seq projectile irony helm-core helm flycheck company clojure-mode avy zenburn-theme yasnippet yaml-mode ws-butler writegood-mode whole-line-or-region web-mode ujelly-theme tronesque-theme temp-buffer-browse symon stripe-buffer string-utils sr-speedbar spacegray-theme soothe-theme solarized-theme smyx-theme smart-mode-line simple-httpd request rainbow-delimiters pyvenv pythonic py-autopep8 puml-mode powerline paredit noctilux-theme names multiple-cursors multi monokai-theme minesweeper material-theme magit lush-theme load-theme-buffer-local kerl json-rpc js2-mode jinja2-mode hungry-delete htmlize highlight-indentation helm-projectile helm-ag haskell-mode grandshell-theme gnugo fountain-mode focus flycheck-package flycheck-irony firecode-theme find-file-in-project expand-region erc-hl-nicks draft-mode diminish deft darktooth-theme cyberpunk-theme corral company-jedi company-irony-c-headers company-irony column-enforce-mode color-theme-sanityinc-tomorrow cider bubbleberry-theme bind-key beacon auto-complete auctex atom-one-dark-theme anchored-transpose aggressive-indent afternoon-theme ace-window @))))
+    (magit-popup list-utils f git-commit command-log-mode smart-comment swiper seq projectile irony helm-core helm flycheck company clojure-mode avy zenburn-theme yasnippet yaml-mode ws-butler writegood-mode whole-line-or-region web-mode ujelly-theme tronesque-theme temp-buffer-browse symon stripe-buffer string-utils sr-speedbar spacegray-theme soothe-theme solarized-theme smyx-theme smart-mode-line simple-httpd request rainbow-delimiters pyvenv pythonic py-autopep8 puml-mode powerline paredit noctilux-theme names multiple-cursors multi monokai-theme minesweeper material-theme magit lush-theme load-theme-buffer-local kerl json-rpc js2-mode jinja2-mode hungry-delete htmlize highlight-indentation helm-projectile helm-ag haskell-mode grandshell-theme gnugo fountain-mode focus flycheck-package flycheck-irony firecode-theme find-file-in-project expand-region erc-hl-nicks draft-mode diminish deft darktooth-theme cyberpunk-theme corral company-jedi company-irony-c-headers company-irony column-enforce-mode color-theme-sanityinc-tomorrow cider bubbleberry-theme bind-key beacon auto-complete auctex atom-one-dark-theme anchored-transpose aggressive-indent afternoon-theme ace-window @)))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 
 (put 'narrow-to-region 'disabled nil)
 (org-babel-load-file
